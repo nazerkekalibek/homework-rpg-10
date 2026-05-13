70f87c69b9c196c1f01afdf0022b6245142e7110
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-05-13 16:41:50 +0500

    Added healer

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -1,8 +1,5 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for wounds, potions, and recovery plans.
- */
 public class Healer extends GuildMember {
 
     public Healer(String name, GuildMediator mediator) {
@@ -10,12 +7,20 @@ public class Healer extends GuildMember {
     }
 
     public void prepareAid(String topic, String payload) {
-        // TODO: send a healing message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        switch(topic){
+            case "orders":
+                System.out.println("[Healer "+getName()+"] received order from "+from.getName()+" -> brewing potions for: "+payload);
+                break;
+            case "healing":
+                System.out.println("[Healer "+getName()+"] coordinating healing with "+from.getName()+" -> "+payload);
+                break;
+            default:
+                System.out.println("[Healer "+getName()+"] heard '"+topic+"' from "+from.getName()+" -> "+payload);
+        }
     }
 }
