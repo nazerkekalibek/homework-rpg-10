5ca4710922f7661312a81fe1d0c5edb3b742b9d8
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-05-13 15:24:55 +0500

    Added quarter master

--- src/com/narxoz/rpg/guild/Quartermaster.java
+++ src/com/narxoz/rpg/guild/Quartermaster.java
@@ -1,8 +1,5 @@
 package com.narxoz.rpg.guild;
 
-/**
- * Guild officer responsible for gear, supplies, and rewards.
- */
 public class Quartermaster extends GuildMember {
 
     public Quartermaster(String name, GuildMediator mediator) {
@@ -10,12 +7,23 @@ public class Quartermaster extends GuildMember {
     }
 
     public void requestSupplies(String topic, String payload) {
-        // TODO: send a supply-related message through the mediator.
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        switch(topic){
+            case "orders":
+                System.out.println("[Quartermaster "+getName()+"] received order from "+from.getName()+" -> preparing supply crates for: "+payload);
+                break;
+            case "scouting":
+                System.out.println("[Quartermaster "+getName()+"] noted scouting report from "+from.getName()+" -> adjusting gear list for: "+payload);
+                break;
+            case "supplies":
+                System.out.println("[Quartermaster "+getName()+"] cross-checking supply note from "+from.getName()+" -> "+payload);
+                break;
+            default:
+                System.out.println("[Quartermaster "+getName()+"] heard '"+topic+"' from "+from.getName()+" -> "+payload);
+        }
     }
 }
